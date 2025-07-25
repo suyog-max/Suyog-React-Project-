28c2dec2433769223b59b17e5cd80ccbf245992b
suyog-max <suyogbhandari93@gmail.com>
2025-07-25 11:00:05 +0545

    fix path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-
-import Home from "./components/Home/Home";
+import Home from "./Components/Home/Home"
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import Login from "./components/Login/Login";
